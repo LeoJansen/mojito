df32514d590767ee0b6468415e42d4a05befdd4d
LeoJansen <leo@leonardojansen.com>
2025-07-11 11:24:41 -0300

    Add GSAP scroll animation to About section
    Introduced GSAP and SplitText animations to the About section. The title and grid elements now animate into view when the section enters the viewport, enhancing the user experience with smooth transitions.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,42 @@
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { SplitText } from "gsap/all";
 
 const About = () => {
+    useGSAP(() => {
+        const titleSplit = SplitText.create("#about h2", {
+            type: 'words'
+        })
+
+        const scrollTimeLine = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#about",
+                start: "top center",
+            }
+        });
+
+        scrollTimeLine.from(titleSplit.words, {
+            yPercent: 100,
+            opacity: 0,
+            stagger: 0.02,
+            duration: 1,
+            ease: "expo.out"
+        })
+        
+        .from(".top-grid, .bottom-grid div", {
+            opacity: 0,
+            stagger: 0.04,
+            duration: 1,
+            ease: "power1.inOut"
+        }, "-=0.5")
+
+
+
+        
+
+
+    }, []);
+
     return (
         <div id="about" >
 
