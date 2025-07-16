450205fe1ac964fbd355d10f5b9c5c825ea24682
Léo Jansen <leo@leonardojansen.com>
2025-07-15 21:50:59 -0300

    Enhance mask animation and adjust list item layout
    Added a fade-in animation for #masked-content in the maskTimeline sequence and removed 'justify-start' from the list item classes for improved layout.

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -21,6 +21,7 @@ const Art = () => {
         maskTimeline
             .to('.will-fade', { opacity: 0, stagger: 0.2, ease: 'power1.inOut', })
             .to('.masked-img', { scale: 1.3, maskPosition:'center', maskSize:'400%', duration: 1, ease: 'power1.inOut' }, '<')
+            .to('#masked-content', { opacity: 1, duration: 1,delay:1, ease: 'power1.inOut' }, '<')
 
 
 
@@ -34,7 +35,7 @@ const Art = () => {
                 <div className="content">
                     <ul className="space-y-4 will-fade">
                         {goodLists.map((feature, index) => (
-                            <li key={index} className="flex items-center justify-start gap-2">
+                            <li key={index} className="flex items-center gap-2">
                                 <img src="/images/check.png" alt="check" />
                                 <p className="md:w-fit w-60">{feature}</p>
                             </li>
