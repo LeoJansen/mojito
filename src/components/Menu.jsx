a0c46bd20922fa5a5bd931ad7332ff13a95bfcba
Léo Jansen <leo@leonardojansen.com>
2025-07-16 08:37:06 -0300

    Refactor cocktail data and update Menu component
    Renamed 'sliderLists' to 'allCocktails' in constants and updated Menu.jsx to use the new name. Added cocktail navigation tabs to the Menu component with active state styling and basic slide navigation logic.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+import { allCocktails } from "../../constants"
 
 
 const Menu = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const goToSlide = (index) =>{
+    const newIndex = index;
+
+  }
+
+
   return (
     <div id="menu" aria-labelledby="menu-heading">
-      <img src="/images/slider-left-leaf.png" alt="left-leaf" className="m-left-leaf" />
-      <img src="/images/slider-right-leaf.png" alt="right-leaf" className="m-right-leaf" />
+      <img src="/images/slider-left-leaf.png" alt="left-leaf" id="m-left-leaf" />
+      <img src="/images/slider-right-leaf.png" alt="right-leaf" id="m-right-leaf" />
+
+      <h2 id="menu-heading" className="sr-only">Cocktail Menu</h2>
+      <nav className="cocktail-tabs" aria-label="Cocktail Navigation">
+        {allCocktails.map((cocktail, index) => {
+          const isActive = index === currentIndex;
+          return (
+            <button id={cocktail.id} 
+            className={`${isActive ? "text-white border-white" : "text-white/50 border-white/50"} cocktail-tab`}
+            onClick={() => goToSlide(index)}
+            >
+              {cocktail.name}
+            </button>
+          )
+        })}
+      </nav>
     </div>
   )
 }
